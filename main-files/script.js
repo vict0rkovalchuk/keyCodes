be38f2ab65d463146f99774bc4e3de1e1fa9e585
Victor <vict02r05@gmail.com>
2021-04-24 00:04:33 +0300

    basic functionality

--- main-files/script.js
+++ main-files/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+let mainValue = document.querySelector(".js-main-numbers"),
+  key = document.querySelector(".text1"),
+  locationRef = document.querySelector(".text2"),
+  which = document.querySelector(".text3"),
+  code = document.querySelector(".text4");
+
+window.addEventListener("keydown", (event) => {
+  mainValue.textContent = event.keyCode;
+  key.textContent = event.key;
+  locationRef.textContent = event.location;
+  which.textContent = event.which;
+  code.textContent = event.code;
+});
